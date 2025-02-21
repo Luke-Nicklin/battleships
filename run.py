8463470866af1c0716ec3a292a33934289bfa0a5
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-02-21 14:59:03 +0000

    Add Board class with variables initialised

--- run.py
+++ run.py
@@ -27,4 +27,52 @@ while True:
         else:
             print("You must enter a number between 5 and 10.")
     except ValueError:
-        print("Erro. You must enter a valid number.")
+        print("Error. You must enter a valid number.")
+
+class Board:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.board = [["." for _ in range(columns)] for _ in range(rows)]
+        self.ships = []
+
+    def display_board(self):
+        print(" ", end="")
+        for i in range(self.columns):
+            print(i + 1, end=" ")
+        print()
+
+        for i in range(self.rows):
+            print(i + 1, end=" ")
+            for j in range(self.columns):
+                print(self.board[i][j], end=" ")
+            print()
+
+player_board = Board(rows, columns)
+computer_board = Board(rows, columns)
+
+print("Your board:")
+player_board.display_board()
+
+print("Computer's board:")
+computer_board.display_board()
+
+while True:
+    try:
+        players_turn = int(input("Choose a column: \n"))
+        if 1 <= rows <= 10:
+            break
+        else:
+            print("You must enter a number between 1 and 10.")
+    except ValueError:
+        print("Error. You must enter a valid number.")
+
+while True:
+    try:
+        players_turn = int(input("Choose a row: \n"))
+        if 1 <= columns <= 10:
+            break
+        else:
+            print("You must enter a number between 1 and 10.")
+    except ValueError:
+        print("Error. You must enter a valid number.")
