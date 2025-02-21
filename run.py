6c42fb7153eb0be5498a1e8fc2138d5232794e51
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-02-21 09:48:43 +0000

    Add ability for user to enter the number of rows and number of columns

--- run.py
+++ run.py
@@ -6,5 +6,25 @@ Battleships welcome message
 
 print("Welcome to Battleships! Let's play...")
 
-class Board:
-    
+"""
+Ask user to select number of rows
+"""
+while True:
+    try:
+        rows = int(input("Enter the number of rows between 5 and 10: \n"))
+        if 5 <= rows <= 10:
+            break
+        else:
+            print("You must enter a number between 5 and 10.")
+    except ValueError:
+        print("Error. You must enter a valid number.")
+
+while True:
+    try:
+        columns = int(input("Enter the number of columns between 5 and 10: \n"))
+        if 5 <= columns <= 10:
+            break
+        else:
+            print("You must enter a number between 5 and 10.")
+    except ValueError:
+        print("Erro. You must enter a valid number.")
